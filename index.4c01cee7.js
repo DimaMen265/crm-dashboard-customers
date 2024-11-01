25fbdaf1a6e938e694ddf8a137575a0f159753fa
DimaMen265 <134741876+DimaMen265@users.noreply.github.com>
2024-11-01 20:36:30 +0000

    Deploying to gh-pages from @ DimaMen265/crm-dashboard-customers@409c9053d2f41f6da15e7deebe961e53e003842f 🚀

---
+++ index.4c01cee7.js
@@ -0,0 +1,2 @@
+document.addEventListener("DOMContentLoaded",(function(){document.querySelectorAll(".fade-in").forEach((function(e,t){setTimeout((function(){e.classList.add("active")}),1e3*t)}))}));
+//# sourceMappingURL=index.4c01cee7.js.map
---
+++ index.761b094e.js
@@ -0,0 +1,2 @@
+document.addEventListener("DOMContentLoaded",(()=>{document.querySelectorAll(".fade-in").forEach(((e,t)=>{setTimeout((()=>{e.classList.add("active")}),1e3*t)}))}));
+//# sourceMappingURL=index.761b094e.js.map
